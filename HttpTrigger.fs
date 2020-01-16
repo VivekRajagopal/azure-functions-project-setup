1c21ff58a94b989175ae6ec2018368ec957a2ca4
unknown <vivrajagopal@gmail.com>
2020-01-16 20:57:17 +1100

    Hello world

---
+++ HttpTrigger.fs
@@ -0,0 +1,14 @@
+namespace POC.Function
+
+open Microsoft.Azure.WebJobs
+open Microsoft.Azure.WebJobs.Extensions.Http
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.Logging
+
+module HttpTrigger =
+    [<FunctionName("HttpTrigger")>]
+    let Run([<HttpTrigger(AuthorizationLevel.Function, "get", Route = null)>] req: HttpRequest , log: ILogger) =    
+        log.LogInformation "C# HTTP trigger function processed a request."
+        let name = req.Query.["name"]
+        name   
+
